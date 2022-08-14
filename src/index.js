49505aa5b2291eb2bea824c93481d7894fc1d45e
Conor Hinchee <conorphinchee@gmail.com>
2022-08-14 12:41:25 -0400

    ball-cube

--- src/index.js
+++ src/index.js
@@ -1,61 +1,92 @@
 import './style/main.css'
-import * as THREE from 'three'
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import Stats from 'stats-js';
+import * as dat from 'dat.gui';
 
-/**
- * Sizes
- */
-const sizes = {}
-sizes.width = window.innerWidth
-sizes.height = window.innerHeight
+const gui = new dat.GUI();
 
-window.addEventListener('resize', () =>
-{
-    // Save sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-})
-
-/**
- * Environnements
- */
-// Scene
-const scene = new THREE.Scene()
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
-
-// Test
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-scene.add(cube)
-
-// Renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('.webgl')
 })
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(sizes.width, sizes.height)
 
-/**
- * Loop
- */
-const loop = () =>
-{
-    // Update
-    cube.rotation.y += 0.01
+renderer.setClearColor(new THREE.Color(0x000000));
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // Render
-    renderer.render(scene, camera)
+//Renderer shadows
+renderer.setClearColor(new THREE.Color(0x000000));
+renderer.shadowMap.enabled = true;
 
-    // Keep looping
-    window.requestAnimationFrame(loop)
+const axes = new THREE.AxesHelper(20);
+scene.add(axes);
+
+const planeGeometry = new THREE.PlaneGeometry(60, 20);
+const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
+
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+plane.position.set(15, 0, 0);
+plane.receiveShadow = true;
+scene.add(plane);
+
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000e });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.position.set(-4, 3, 0);
+cube.castShadow = true;
+scene.add(cube);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(20, 4, 2);
+sphere.castShadow = true;
+scene.add(sphere);
+
+const spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(-40, 40, -15);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+spotLight.shadow.camera.near = 40;
+spotLight.shadow.camera.far = 130;
+scene.add(spotLight);
+
+camera.position.set(-30, 40, 30);
+camera.lookAt(scene.position);
+
+let step = 0; 
+
+const stats = new Stats();
+stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild( stats.dom );
+
+const controls = new function () {
+    this.rotationSpeed = 0.02;
+    this.bouncingSpeed = 0.02;
 }
-loop()
+
+gui.add(controls, 'rotationSpeed', 0, 0.5);
+gui.add(controls, 'bouncingSpeed', 0, 0.5);
+
+const orbitControls = new OrbitControls( camera, renderer.domElement );
+orbitControls.update();
+
+function renderScene() {
+    stats.begin();
+    requestAnimationFrame(renderScene);
+    cube.rotation.x += controls.rotationSpeed;
+    cube.rotation.y += controls.rotationSpeed;
+    cube.rotation.z += controls.rotationSpeed;
+
+    step += controls.bouncingSpeed;
+    sphere.position.x = 20 + (10 * (Math.cos(step)));
+    sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+    
+    orbitControls.update();
+    renderer.render(scene, camera);
+    stats.end();
+}
+
+renderScene();
